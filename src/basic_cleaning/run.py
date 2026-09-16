2c0aa25e90405b0a2eff017dba9978f528dc879e
Victor Nguyen <victornguyen@chevron.com>
2026-09-15 21:48:57 -0500

    updated basic_cleaning per feedback - updated last_review to datetime type - added description to mlproject

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -45,6 +45,9 @@ def go(args):
     idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
     df = df[idx].copy()
 
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
     logger.info("Cleaned data has %s rows and %s columns", *df.shape)
 
     # Save the cleaned data to a new CSV file
